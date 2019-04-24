a3d9dd490dab63f22a06ca9bf5e26bd7b96bd610
Lucmie98 <lucmie98@gmail.com>
2019-04-25 00:23:53 +0200

    Rahmen angepasst, Hintergrundbild testweise eingefügt, Status bei all_tickets_item.xml hinzugefügt und Inhalt für Spinner angelegt

--- app/src/main/java/com/example/emi/All_tickets.java
+++ app/src/main/java/com/example/emi/All_tickets.java
@@ -1,7 +1,10 @@
 package com.example.emi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class All_tickets extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class All_tickets extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_tickets);
+
+        ImageView back_arrow = (ImageView) findViewById(R.id.back_arrow);
+        back_arrow.setVisibility(View.INVISIBLE);
+
+        ImageView house = (ImageView) findViewById(R.id.home);
+        house.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backToHome = new Intent(All_tickets.this, MainActivity.class);
+                startActivity(backToHome);
+            }
+        });
+
     }
 }
