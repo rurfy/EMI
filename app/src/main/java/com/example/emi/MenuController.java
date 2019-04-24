e278da9577fbaf27951e39eb148f6e8fd9c34d65
rurfy <chrissy.richter@web.de>
2019-04-24 16:09:07 +0200

    API Connector erstellt

--- app/src/main/java/com/example/emi/MenuController.java
+++ app/src/main/java/com/example/emi/MenuController.java
@@ -1,4 +1,19 @@
 package com.example.emi;
 
-public class MenuController {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MenuController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedBundle) {
+        super.onCreate(savedBundle);
+        setContentView(R.layout.main_menu);
+
+        Button createTicket = findViewById(R.id.createTicketButton);
+        Button allTickets = findViewById(R.id.viewAllTicketsButton);
+
+
+    }
 }
