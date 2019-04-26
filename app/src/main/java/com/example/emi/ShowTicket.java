a87ee2410127aad9b39f6a95b288c89f65a4421e
Marie <mariebe.99@gmx.de>
2019-04-26 23:56:23 +0200

    TWEAK ShowTicket Controller begonnen

--- app/src/main/java/com/example/emi/ShowTicket.java
+++ app/src/main/java/com/example/emi/ShowTicket.java
@@ -2,12 +2,40 @@ package com.example.emi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShowTicket extends AppCompatActivity {
+
+    Button erstellenButton;
+    EditText inputTitel;
+    EditText inputErsteller;
+    EditText inputProblembeschreibung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_ticket);
+
+        Map ticketData = new HashMap();
+
+        //getTicket(id);
+
+        inputTitel = (EditText)findViewById(R.id.textInputEditTextTitel);
+        inputTitel.setEnabled(false);
+        inputTitel.setText(ticketData.get("Titel").toString());
+
+        inputErsteller = (EditText)findViewById(R.id.textInputEditTextTitel);
+        inputErsteller.setEnabled(false);
+        inputErsteller.setText(ticketData.get("Ersteller").toString());
+
+        inputProblembeschreibung = (EditText)findViewById(R.id.textInputEditTextTitel);
+        inputProblembeschreibung.setEnabled(false);
+        inputProblembeschreibung.setText(ticketData.get("Problembeschreibung").toString());
+
     }
 }
