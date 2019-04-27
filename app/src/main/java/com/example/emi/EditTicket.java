ce3a36536137f3bb12946c5527d48e68b5228e40
Marie <mariebe.99@gmx.de>
2019-04-27 12:21:26 +0200

    TWEAK Datum, Kategorie

--- app/src/main/java/com/example/emi/EditTicket.java
+++ app/src/main/java/com/example/emi/EditTicket.java
@@ -2,13 +2,16 @@ package com.example.emi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,18 +36,27 @@ public class EditTicket extends AppCompatActivity {
             buttonCancel.setText(R.string.cancel);
 
 
-            // TODO Kategorie-String aus Datenbank
-            // Drop-Down-Liste füllen
+            // TODO getKategories() -> String[][]
+            final String[][] test2 = new String[3][2];
+            test2[0][0] = "2";
+            test2[0][1] = "test";
+            test2[1][0] = "5";
+            test2[1][1] = "test2";
+
             List<String> test = new ArrayList<String>();
-            test.add("test1");
-            test.add("test2");
+
+            for (int i = 0; i<test2.length; i++) {
+
+                test.add(test2[i][1]);
+            }
+            // Drop-Down-Liste füllen
             spinnerCategory = (Spinner) findViewById(R.id.spinnerCategory);
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, test);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinnerCategory.setAdapter(adapter);
 
 
-            // TODO getTicket(id);
+            // TODO getTicket(id) -> Hashmap;
             // Testmap
             Map ticketData = new HashMap();
             ticketData.put("Titel", "TestTitel");
@@ -67,6 +79,15 @@ public class EditTicket extends AppCompatActivity {
                     inputTitel = (EditText)findViewById(R.id.textInputEditTextTitel);
                     inputCreator = (EditText)findViewById(R.id.textInputEditTextCreator);
                     inputProblem = (EditText)findViewById(R.id.textInputEditTextProblem);
+
+                    int categoryPosition = spinnerCategory.getSelectedItemPosition();
+                    String categoryId = test2[categoryPosition][0];
+
+
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+                    Date currentDate = new Date();
+                    String date = formatter.format(currentDate);
+                    Log.d("Date", date);
 
                     //
                 }
