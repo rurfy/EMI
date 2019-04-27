20a0d7c0731c0d939f9034d00dd5dc795fcec71c
Marie <mariebe.99@gmx.de>
2019-04-27 02:35:49 +0200

    TWEAK Buttons, Felder

--- app/src/main/java/com/example/emi/CreateTicket.java
+++ app/src/main/java/com/example/emi/CreateTicket.java
@@ -3,30 +3,55 @@ package com.example.emi;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateTicket extends AppCompatActivity {
 
-        Button erstellenButton;
+        Button buttonCreate;
+        Button buttonCancel;
         EditText inputTitel;
-        EditText inputErsteller;
-        EditText inputProblembeschreibung;
+        EditText inputCreator;
+        EditText inputProblem;
+        Spinner spinnerCategory;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.show_ticket);
 
-            erstellenButton = (Button)findViewById(R.id.buttonErstellen);
-            erstellenButton.setOnClickListener(new View.OnClickListener() {
+            buttonCreate = (Button)findViewById(R.id.buttonLeft);
+            buttonCreate.setText(R.string.ok);
+            buttonCancel = (Button) findViewById(R.id.buttonRight);
+            buttonCancel.setText(R.string.cancel);
+
+
+            // TODO Kategorie-String aus Datenbank
+            // Drop-Down-Liste füllen
+            List<String> test = new ArrayList<String>();
+            test.add("test1");
+            test.add("test2");
+            spinnerCategory = (Spinner) findViewById(R.id.spinnerCategory);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, test);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinnerCategory.setAdapter(adapter);
+
+
+
+            buttonCreate.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    inputTitel = (EditText)findViewById(R.id.textInputEditTextTitel);
-                    inputErsteller = (EditText)findViewById(R.id.textInputEditTextErsteller);
-                    inputProblembeschreibung = (EditText)findViewById(R.id.textInputEditTextProblembeschreibung);
 
-                    //
+                    // Daten aus den Feldern auslesen
+                    inputTitel = (EditText)findViewById(R.id.textInputEditTextTitel);
+                    inputCreator = (EditText)findViewById(R.id.textInputEditTextCreator);
+                    inputProblem = (EditText)findViewById(R.id.textInputEditTextProblem);
+
                 }
             });
         }
