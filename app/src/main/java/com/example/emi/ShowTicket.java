604cc74c145f6ea54b296a41394d95419635ff0c
Marie <mariebe.99@gmx.de>
2019-04-27 13:29:21 +0200

    TWEAK Namen, Kategorie anzeigen

--- app/src/main/java/com/example/emi/ShowTicket.java
+++ app/src/main/java/com/example/emi/ShowTicket.java
@@ -2,18 +2,21 @@ package com.example.emi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ShowTicket extends AppCompatActivity {
 
     Button buttonEdit;
     Button buttonBack;
-    EditText inputTitel;
+    EditText inputTitle;
     EditText inputCreator;
     EditText inputProblem;
     Spinner spinnerCategory;
@@ -33,14 +36,15 @@ public class ShowTicket extends AppCompatActivity {
         ticketData.put("Titel", "TestTitel");
         ticketData.put("Ersteller", "Marie Berger");
         ticketData.put("Problembeschreibung", "ist kaputt");
+        ticketData.put("Kategorie", "2");
 
         // TODO Daten aus DB holen;
 
 
         // Werte aus der DB in die Felder eintragen und Felder sperren
-        inputTitel = (EditText)findViewById(R.id.textInputEditTextTitel);
-        inputTitel.setEnabled(false);
-        inputTitel.setText(ticketData.get("Titel").toString());
+        inputTitle = (EditText)findViewById(R.id.textInputEditTextTitle);
+        inputTitle.setEnabled(false);
+        inputTitle.setText(ticketData.get("Titel").toString());
 
         inputCreator = (EditText)findViewById(R.id.textInputEditTextCreator);
         inputCreator.setEnabled(false);
@@ -50,12 +54,40 @@ public class ShowTicket extends AppCompatActivity {
         inputProblem.setEnabled(false);
         inputProblem.setText(ticketData.get("Problembeschreibung").toString());
 
+
+
+
+        // TODO getKategories() -> String[][]
+        final String[][] test2 = new String[3][2];
+        test2[0][0] = "2";
+        test2[0][1] = "test";
+        test2[1][0] = "5";
+        test2[1][1] = "test2";
+
+        List<String> test = new ArrayList<String>();
+
+        for (int i = 0; i<test2.length; i++) {
+
+            if (test2[i][1] == null) {
+                break;
+            }else {
+                test.add(test2[i][1]);
+            }
+        }
+        // Drop-Down-Liste füllen
         spinnerCategory = (Spinner) findViewById(R.id.spinnerCategory);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, test);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(adapter);
         spinnerCategory.setEnabled(false);
         spinnerCategory.setClickable(false);
-        // TODO wie bestehende Kategorie anzeigen?
+        // Wert aus DB in DropDown auswählen
+        for (int i = 0; i < test2.length; i++) {
 
-
-
+            if (test2[i][0].equals((ticketData.get("Kategorie").toString()))) {
+                spinnerCategory.setSelection(i);
+                break;
+            }
+        }
     }
 }
