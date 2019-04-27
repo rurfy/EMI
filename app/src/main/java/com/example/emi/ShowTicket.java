673bf15c8731cf5440d4532d277167892aff2c8c
Marie <mariebe.99@gmx.de>
2019-04-27 02:34:57 +0200

    TWEAK Buttons, Felder, Kategorie fehlt

--- app/src/main/java/com/example/emi/ShowTicket.java
+++ app/src/main/java/com/example/emi/ShowTicket.java
@@ -2,40 +2,60 @@ package com.example.emi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ShowTicket extends AppCompatActivity {
 
-    Button erstellenButton;
+    Button buttonEdit;
+    Button buttonBack;
     EditText inputTitel;
-    EditText inputErsteller;
-    EditText inputProblembeschreibung;
+    EditText inputCreator;
+    EditText inputProblem;
+    Spinner spinnerCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_ticket);
 
+        buttonEdit = (Button) findViewById(R.id.buttonLeft);
+        buttonEdit.setText(R.string.edit);
+        buttonBack = (Button) findViewById(R.id.buttonRight);
+        buttonBack.setText(R.string.back);
+
+        // Testmap
         Map ticketData = new HashMap();
+        ticketData.put("Titel", "TestTitel");
+        ticketData.put("Ersteller", "Marie Berger");
+        ticketData.put("Problembeschreibung", "ist kaputt");
 
-        //getTicket(id);
+        // TODO Daten aus DB holen;
 
+
+        // Werte aus der DB in die Felder eintragen und Felder sperren
         inputTitel = (EditText)findViewById(R.id.textInputEditTextTitel);
         inputTitel.setEnabled(false);
         inputTitel.setText(ticketData.get("Titel").toString());
 
-        inputErsteller = (EditText)findViewById(R.id.textInputEditTextTitel);
-        inputErsteller.setEnabled(false);
-        inputErsteller.setText(ticketData.get("Ersteller").toString());
+        inputCreator = (EditText)findViewById(R.id.textInputEditTextCreator);
+        inputCreator.setEnabled(false);
+        inputCreator.setText(ticketData.get("Ersteller").toString());
 
-        inputProblembeschreibung = (EditText)findViewById(R.id.textInputEditTextTitel);
-        inputProblembeschreibung.setEnabled(false);
-        inputProblembeschreibung.setText(ticketData.get("Problembeschreibung").toString());
+        inputProblem = (EditText)findViewById(R.id.textInputEditTextProblem);
+        inputProblem.setEnabled(false);
+        inputProblem.setText(ticketData.get("Problembeschreibung").toString());
+
+        spinnerCategory = (Spinner) findViewById(R.id.spinnerCategory);
+        spinnerCategory.setEnabled(false);
+        spinnerCategory.setClickable(false);
+        // TODO wie bestehende Kategorie anzeigen?
+
+
 
     }
 }
