eed83fb44a34be9e7aac1e480ac9dd313c7ee4a0
Christopher Richter <chrissy.richter@web.de>
2019-04-29 00:03:46 +0200

    PostMethode hinzugefügt

--- app/src/main/java/com/example/emi/APIConnector.java
+++ app/src/main/java/com/example/emi/APIConnector.java
@@ -1,8 +1,11 @@
 package com.example.emi;
 
+import android.content.Context;
 import android.util.Log;
 
 import com.loopj.android.http.*;
+
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class APIConnector {
     private static final String BASE_URL = "https://steffen.cloud/api/";
@@ -13,10 +16,10 @@ public class APIConnector {
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
-    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+    public static void post(Context context, String url, StringEntity entity, String contentType, AsyncHttpResponseHandler responseHandler) {
         client.addHeader("Accept", "application/json");
         client.addHeader("Content-type", "application/json");
-        client.post(getAbsoluteUrl(url), params, responseHandler);
+        client.post(context, getAbsoluteUrl(url), entity, contentType, responseHandler);
     }
 
     private static String getAbsoluteUrl(String relativeUrl) {
